c8de97cef5abb2f5361797c6b8721cebb7699583
hussien-m <hussienalfeqawy@gmail.com>
2023-03-10 22:39:07 +0200

    Change info

--- resources/views/layouts/dashboard/dashboard.blade.php
+++ resources/views/layouts/dashboard/dashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8">
-    <title>{{ $option->site_name ??  " | ". 'لوحة التحكم' }}</title>
+    <title>{{ app()->getLocale() == 'ar' ? 'لوحة التحكم | اويا بيتش': 'Dashboard|Oia beach' }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
